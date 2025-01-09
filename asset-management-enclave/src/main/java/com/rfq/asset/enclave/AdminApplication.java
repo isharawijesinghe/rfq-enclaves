9d0b06d1dcb2b2264d9253bcc0e3d8b84922d0d0
IsharaWijesinghe <ishara.wijesinghe@trustpayments.com>
2025-01-09 18:42:01 +0530

    Initial commit added

---
+++ asset-management-enclave/src/main/java/com/rfq/asset/enclave/AdminApplication.java
@@ -0,0 +1,20 @@
+package com.rfq.asset.enclave;
+
+import com.rfq.enclave.autoconfigure.EnableNitroEnclavesEnclaveSide;
+import com.rfq.enclave.enclave.server.NitroEnclaveServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableNitroEnclavesEnclaveSide
+public class AdminApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(AdminApplication.class, args);
+		NitroEnclaveServer server = ctx.getBean(NitroEnclaveServer.class);
+		server.run();
+	}
+
+}
